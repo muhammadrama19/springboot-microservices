7f8aa83926e513aff9d80d1565dd3d0dcc8ff7a3
Muhammad Rama Nurimani <124318936+muhammadrama19@users.noreply.github.com>
2025-02-14 15:16:42 +0700

    add inventory services

---
+++ inventory-service/src/main/java/com/techie/microservices/inventory/controller/InventoryController.java
@@ -0,0 +1,27 @@
+package com.techie.microservices.inventory.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+
+import com.techie.microservices.inventory.service.InventoryService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/inventory")
+@RequiredArgsConstructor
+
+public class InventoryController {
+    private final InventoryService inventoryService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity) {
+        return inventoryService.isInStock(skuCode, quantity);
+    }
+
+}
